3642039ea4983d1acd0b149c21b5115e17a6b79e
Matt <brasil.mateus748@gmail.com>
2023-07-18 03:34:00 -0300

    Create a function to play a single round Create a function to play five rounds Create conditionals to announce the winner and keep track of the score

--- script.js
+++ script.js
@@ -1,13 +1,59 @@
+// Create function to randomly generate Rock, Paper or Scissors
 
-function getComputerchoice() {
-    let randomChoice = Math.random() * 3;    
-    if (randomChoice <= 1) { 
-        randomChoice = "Rock"
-    } else if (randomChoice <= 2) {      // Create a function to return randomly 'Paper', 'Rock' or 'Paper'
-        randomChoice = "Scissors"
-    } else { 
-        randomChoice = "Paper"
-    };
-    return randomChoice;
+let playerScore = 0;
+let cpuScore = 0;
+
+function getComputerChoice(){
+    let computerChoice = ["Rock", "Paper", "Scissors"];
+    return computerChoice[Math.floor(Math.random() * computerChoice.length)]; 
+    
 }
-console.log(getComputerchoice());
+
+// Create function to play a single round
+
+function playRound(playerSelection, computerSelection){
+    
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+     } else if (playerSelection === "Rock" && computerSelection === "Scissors") { 
+        playerScore+=1;
+        return "You win! Rock beats Scissors";
+     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        playerScore+=1;
+        return "You win! Paper beats Rock";
+     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        playerScore+=1;
+        return "You win! Scissors beats Paper";
+     } else {
+        cpuScore+=1;
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+     }
+     
+  }
+  
+
+
+  // Create a function to play five rounds and keep track of the score
+
+function game(){
+    for (let i = 0; i < 5; i++){
+    const playerSelection = prompt("What is your choice?");
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    }
+}
+  game();
+
+  if(playerScore){
+    console.log("Player:"+" "+ playerScore +" "+"CPU:"+" "+ cpuScore);
+  } else if(cpuScore){
+    console.log("Player:"+" "+ playerScore +" "+"CPU:"+" "+ cpuScore);
+  }
+
+  if(playerScore > cpuScore){
+    alert("Player is the winner!");
+} else if(playerScore < cpuScore) {
+    alert("Computer is the winner!");
+} else {
+    alert("It's a tie!");
+};
