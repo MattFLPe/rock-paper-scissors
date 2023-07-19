dacc5f9d33ba0d2534862bb4bf0b7ea6347667f8
Matt <brasil.mateus748@gmail.com>
2023-07-18 22:12:14 -0300

    Fix parameter case-insensitive Users now can input variations: 'ROCK', rock, 'RocK', etc

--- script.js
+++ script.js
@@ -4,36 +4,35 @@ let playerScore = 0;
 let cpuScore = 0;
 
 function getComputerChoice(){
-    let computerChoice = ["Rock", "Paper", "Scissors"];
+    let computerChoice = ["rock", "paper", "scissors"];
     return computerChoice[Math.floor(Math.random() * computerChoice.length)]; 
     
 }
 
-// Create function to play a single round
+// Create a function to play a single round
 
 function playRound(playerSelection, computerSelection){
     
-    if (playerSelection === computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
         return "It's a tie!";
-     } else if (playerSelection === "Rock" && computerSelection === "Scissors") { 
+     } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") { 
         playerScore+=1;
         return "You win! Rock beats Scissors";
-     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+     } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock") {
         playerScore+=1;
         return "You win! Paper beats Rock";
-     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
         playerScore+=1;
         return "You win! Scissors beats Paper";
      } else {
         cpuScore+=1;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
      }
-     
   }
   
+  
 
-
-  // Create a function to play five rounds and keep track of the score
+  // Create a function to play five rounds 
 
 function game(){
     for (let i = 0; i < 5; i++){
@@ -41,8 +40,11 @@ function game(){
     const computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
     }
+    
 }
   game();
+
+  // Keep track of score and announces the winner
 
   if(playerScore){
     console.log("Player:"+" "+ playerScore +" "+"CPU:"+" "+ cpuScore);
